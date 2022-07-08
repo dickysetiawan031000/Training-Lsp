794d5f3171ebb09c362e2f8766dd57ba97278d3e
Dicky Setiawan <dickysetiawan031000@gmail.com>
2022-07-07 22:37:49 -0700

    add middleware in Kernel and Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\auth\LoginController;
+use App\Http\Controllers\auth\RegisterController;
+use App\Http\Controllers\customer\DashboardController as CustomerDashboardController;
 use App\Http\Controllers\front\DashboardController as FrontDashboardController;
 use App\Models\Buy;
 use App\Models\Customer;
@@ -28,16 +31,31 @@ use Illuminate\Support\Facades\Route;
 
 //Front
 Route::resource('/', FrontDashboardController::class);
-
+Route::resource('/register', RegisterController::class);
+Route::resource('/login', LoginController::class);
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
 //Admin
+Route::group(
+    ['middleware' => ['auth', 'checkRole:1']],
+    function () {
 
-Route::prefix('admin')->name('admin.')->group(function () {
+        Route::prefix('admin')->name('admin.')->group(function () {
 
-    Route::resource('/product', ProductController::class);
-    Route::resource('/dashboard', DashboardController::class);
-});
-
-
+            Route::resource('/product', ProductController::class);
+            Route::resource('/dashboard', DashboardController::class);
+        });
+    }
+);
 
 //Customer
+Route::group(
+    ['middleware' => ['auth', 'checkRole:2']],
+    function () {
+
+        Route::prefix('customer')->name('customer.')->group(function () {
+            Route::resource('/dashboard', CustomerDashboardController::class);
+        });
+    }
+);
